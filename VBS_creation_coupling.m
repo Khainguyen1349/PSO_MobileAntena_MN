386bdb67cf1cdfb25915663bd1c54f33f8be265d
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-09-16 10:14:14 +0200

    Update VBS_creation_coupling.m
    Utilizing Struct for antenna geometry variables

--- VBS_creation_coupling.m
+++ VBS_creation_coupling.m
@@ -1,9 +1,9 @@
-function []=VBS_creation_coupling(gndx,gndy,patchy,patchh, feed_pos1, feed_pos2,feed_linew,gap_pos,fC,fLow,fHigh,nPoints,tmpPrjFile, tmpDataFile, PEC)
+function []=VBS_creation_coupling(Antenna,tmpPrjFile, tmpDataFile)
 
-% Variable PEC : 1 if PEC, 0 if Finite conductivity
+% Variable Antenna.PEC : 1 if Antenna.PEC, 0 if Finite conductivity
 
 %lecture points
- lambda = 1000*3e8/fC; % in mm
+ lambda = 1000*3e8/Antenna.fC; % in mm
  
  tmpRadFile = [pwd, '\tmpRad.m'];
 
@@ -23,14 +23,14 @@ fprintf(fid,'\n');
 
 %GND plane
 fprintf(fid,'oEditor.CreateRectangle Array("NAME:RectangleParameters", "IsCovered:=", true, "XStart:=",  _\n');
-fprintf(fid,'  "-%fmm", "YStart:=", "%dmm", "ZStart:=", "0mm", "Width:=", "%fmm", "Height:=",  _\n',gndx/2,0,gndx);
-fprintf(fid,'  "%fmm", "WhichAxis:=", "Z"), Array("NAME:Attributes", "Name:=", "Ground", "Flags:=",  _\n',gndy);
+fprintf(fid,'  "-%fmm", "YStart:=", "%dmm", "ZStart:=", "0mm", "Width:=", "%fmm", "Height:=",  _\n',Antenna.gndx/2,Antenna.gnd_clearance,Antenna.gndx);
+fprintf(fid,'  "%fmm", "WhichAxis:=", "Z"), Array("NAME:Attributes", "Name:=", "Ground", "Flags:=",  _\n',Antenna.gndy-Antenna.gnd_clearance);
 fprintf(fid,'  "", "Color:=", "(132 132 193)", "Transparency:=", 0, "PartCoordinateSystem:=",  _\n');
 fprintf(fid,'  "Global", "UDMId:=", "", "MaterialValue:=", "" & Chr(34) & "vacuum" & Chr(34) & "", "SolveInside:=",  _\n');
 fprintf(fid,'  true)\n');
 
 fprintf(fid,'Set oModule = oDesign.GetModule("BoundarySetup")\n');
-if(PEC==1)
+if(Antenna.PEC==1)
 fprintf(fid,'oModule.AssignPerfectE Array("NAME:PerfE1", "Objects:=", Array("Ground"  _\n');
 fprintf(fid,' ), "InfGroundPlane:=", false)\n');
 else
@@ -41,14 +41,14 @@ end
 
 %patch1
 fprintf(fid,'oEditor.CreateRectangle Array("NAME:RectangleParameters", "IsCovered:=", true, "XStart:=",  _\n');
-fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%dmm", "Width:=", "%fmm", "Height:=",  _\n',-gndx/2,0,patchh+0.5, gndx);
-fprintf(fid,'  "%fmm", "WhichAxis:=", "Z"), Array("NAME:Attributes", "Name:=", "Patch1", "Flags:=",  _\n',patchy);
+fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%dmm", "Width:=", "%fmm", "Height:=",  _\n',-Antenna.gndx/2,0,Antenna.patchh+0.5, Antenna.gndx);
+fprintf(fid,'  "%fmm", "WhichAxis:=", "Z"), Array("NAME:Attributes", "Name:=", "Patch1", "Flags:=",  _\n',Antenna.patchy);
 fprintf(fid,'  "", "Color:=", "(132 132 193)", "Transparency:=", 0, "PartCoordinateSystem:=",  _\n');
 fprintf(fid,'  "Global", "UDMId:=", "", "MaterialValue:=", "" & Chr(34) & "vacuum" & Chr(34) & "", "SolveInside:=",  _\n');
 fprintf(fid,'  true)\n');
 
 fprintf(fid,'Set oModule = oDesign.GetModule("BoundarySetup")\n');
-if(PEC==1)
+if(Antenna.PEC==1)
 fprintf(fid,'oModule.AssignPerfectE Array("NAME:PerfE2", "Objects:=", Array("Patch1"  _\n');
 fprintf(fid,' ), "InfGroundPlane:=", false)\n');
 else
@@ -59,8 +59,8 @@ end
 
 %gap
 fprintf(fid,'oEditor.CreateRectangle Array("NAME:RectangleParameters", "IsCovered:=", true, "XStart:=",  _\n');
-fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%dmm", "Width:=", "%fmm", "Height:=",  _\n',-gndx/2+gap_pos,0,patchh+0.5, 1);
-fprintf(fid,'  "%fmm", "WhichAxis:=", "Z"), Array("NAME:Attributes", "Name:=", "Gap", "Flags:=",  _\n',patchy);
+fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%dmm", "Width:=", "%fmm", "Height:=",  _\n',Antenna.gap_pos - Antenna.gapw/2,0,Antenna.patchh+0.5, Antenna.gapw);
+fprintf(fid,'  "%fmm", "WhichAxis:=", "Z"), Array("NAME:Attributes", "Name:=", "Gap", "Flags:=",  _\n',Antenna.patchy);
 fprintf(fid,'  "", "Color:=", "(132 132 193)", "Transparency:=", 0, "PartCoordinateSystem:=",  _\n');
 fprintf(fid,'  "Global", "UDMId:=", "", "MaterialValue:=", "" & Chr(34) & "vacuum" & Chr(34) & "", "SolveInside:=",  _\n');
 fprintf(fid,'  true)\n');
@@ -69,14 +69,14 @@ hfssSubtract(fid, {'Patch1'}, {'Gap'});
 
 %Vertical line 1
 fprintf(fid,'oEditor.CreateRectangle Array("NAME:RectangleParameters", "IsCovered:=", true, "XStart:=",  _\n');
-fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%dmm", "Width:=", "%fmm", "Height:=",  _\n',feed_pos1,0,0.5, patchh);
-fprintf(fid,'  "%fmm", "WhichAxis:=", "Y"), Array("NAME:Attributes", "Name:=", "Verline1", "Flags:=",  _\n',feed_linew);
+fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%dmm", "Width:=", "%fmm", "Height:=",  _\n',Antenna.feed_pos1 - Antenna.feed_linew/2,Antenna.gnd_clearance,0.5, Antenna.patchh);
+fprintf(fid,'  "%fmm", "WhichAxis:=", "Y"), Array("NAME:Attributes", "Name:=", "Verline1", "Flags:=",  _\n',Antenna.feed_linew);
 fprintf(fid,'  "", "Color:=", "(132 132 193)", "Transparency:=", 0, "PartCoordinateSystem:=",  _\n');
 fprintf(fid,'  "Global", "UDMId:=", "", "MaterialValue:=", "" & Chr(34) & "vacuum" & Chr(34) & "", "SolveInside:=",  _\n');
 fprintf(fid,'  true)\n');
 
 fprintf(fid,'Set oModule = oDesign.GetModule("BoundarySetup")\n');
-if(PEC==1)
+if(Antenna.PEC==1)
 fprintf(fid,'oModule.AssignPerfectE Array("NAME:PerfE4", "Objects:=", Array("Verline1"  _\n');
 fprintf(fid,' ), "InfGroundPlane:=", false)\n');
 else
@@ -87,14 +87,14 @@ end
 
 %Vertical line 2
 fprintf(fid,'oEditor.CreateRectangle Array("NAME:RectangleParameters", "IsCovered:=", true, "XStart:=",  _\n');
-fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%dmm", "Width:=", "%fmm", "Height:=",  _\n',feed_pos2,0,0.5, patchh);
-fprintf(fid,'  "%fmm", "WhichAxis:=", "Y"), Array("NAME:Attributes", "Name:=", "Verline2", "Flags:=",  _\n',feed_linew);
+fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%dmm", "Width:=", "%fmm", "Height:=",  _\n',Antenna.feed_pos2 - Antenna.feed_linew/2,Antenna.gnd_clearance,0.5, Antenna.patchh);
+fprintf(fid,'  "%fmm", "WhichAxis:=", "Y"), Array("NAME:Attributes", "Name:=", "Verline2", "Flags:=",  _\n',Antenna.feed_linew);
 fprintf(fid,'  "", "Color:=", "(132 132 193)", "Transparency:=", 0, "PartCoordinateSystem:=",  _\n');
 fprintf(fid,'  "Global", "UDMId:=", "", "MaterialValue:=", "" & Chr(34) & "vacuum" & Chr(34) & "", "SolveInside:=",  _\n');
 fprintf(fid,'  true)\n');
 
 fprintf(fid,'Set oModule = oDesign.GetModule("BoundarySetup")\n');
-if(PEC==1)
+if(Antenna.PEC==1)
 fprintf(fid,'oModule.AssignPerfectE Array("NAME:PerfE5", "Objects:=", Array("Verline2"  _\n');
 fprintf(fid,' ), "InfGroundPlane:=", false)\n');
 else
@@ -105,8 +105,8 @@ end
 
 %Port 1
 fprintf(fid,'oEditor.CreateRectangle Array("NAME:RectangleParameters", "IsCovered:=", true, "XStart:=",  _\n');
-fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%fmm", "Width:=", "%fmm", "Height:=",  _\n',feed_pos1,0,0,0.5);
-fprintf(fid,'  "%fmm", "WhichAxis:=", "Y"), Array("NAME:Attributes", "Name:=", "Feed1", "Flags:=",  _\n',feed_linew);
+fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%fmm", "Width:=", "%fmm", "Height:=",  _\n',Antenna.feed_pos1 - Antenna.feed_linew/2,Antenna.gnd_clearance,0,0.5);
+fprintf(fid,'  "%fmm", "WhichAxis:=", "Y"), Array("NAME:Attributes", "Name:=", "Feed1", "Flags:=",  _\n',Antenna.feed_linew);
 fprintf(fid,'  "", "Color:=", "(132 132 193)", "Transparency:=", 0, "PartCoordinateSystem:=",  _\n');
 fprintf(fid,'  "Global", "UDMId:=", "", "MaterialValue:=", "" & Chr(34) & "vacuum" & Chr(34) & "", "SolveInside:=",  _\n');
 fprintf(fid,'  true)\n');
@@ -114,15 +114,15 @@ fprintf(fid,'  true)\n');
 
 fprintf(fid,'oModule.AssignLumpedPort Array("NAME:1", "Objects:=", Array("Feed1"), "RenormalizeAllTerminals:=",  _\n');
 fprintf(fid,'  true, "DoDeembed:=", false, Array("NAME:Modes", Array("NAME:Mode1", "ModeNum:=", 1, "UseIntLine:=",  _\n');
-fprintf(fid,'  true, Array("NAME:IntLine", "Start:=", Array("%fmm", "0mm", "0mm"), "End:=", Array( _\n',feed_pos1+feed_linew/2);
-fprintf(fid,'  "%fmm", "0mm", "%fmm")), "CharImp:=", "Zpi", "AlignmentGroup:=", 0)), "ShowReporterFilter:=",  _\n',feed_pos1+feed_linew/2, 0.5);
+fprintf(fid,'  true, Array("NAME:IntLine", "Start:=", Array("%fmm", "%dmm", "0mm"), "End:=", Array( _\n',Antenna.feed_pos1,Antenna.gnd_clearance);
+fprintf(fid,'  "%fmm", "%dmm", "%fmm")), "CharImp:=", "Zpi", "AlignmentGroup:=", 0)), "ShowReporterFilter:=",  _\n',Antenna.feed_pos1,Antenna.gnd_clearance,0.5);
 fprintf(fid,'  false, "ReporterFilter:=", Array(true), "FullResistance:=", "50ohm", "FullReactance:=",  _\n');
 fprintf(fid,'  "0ohm")\n');
 
 %Port 2
 fprintf(fid,'oEditor.CreateRectangle Array("NAME:RectangleParameters", "IsCovered:=", true, "XStart:=",  _\n');
-fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%fmm", "Width:=", "%fmm", "Height:=",  _\n',feed_pos2,0,0,0.5);
-fprintf(fid,'  "%fmm", "WhichAxis:=", "Y"), Array("NAME:Attributes", "Name:=", "Feed2", "Flags:=",  _\n',feed_linew);
+fprintf(fid,'  "%fmm", "YStart:=", "%dmm", "ZStart:=", "%fmm", "Width:=", "%fmm", "Height:=",  _\n',Antenna.feed_pos2 - Antenna.feed_linew/2,Antenna.gnd_clearance,0,0.5);
+fprintf(fid,'  "%fmm", "WhichAxis:=", "Y"), Array("NAME:Attributes", "Name:=", "Feed2", "Flags:=",  _\n',Antenna.feed_linew);
 fprintf(fid,'  "", "Color:=", "(132 132 193)", "Transparency:=", 0, "PartCoordinateSystem:=",  _\n');
 fprintf(fid,'  "Global", "UDMId:=", "", "MaterialValue:=", "" & Chr(34) & "vacuum" & Chr(34) & "", "SolveInside:=",  _\n');
 fprintf(fid,'  true)\n');
@@ -130,23 +130,23 @@ fprintf(fid,'  true)\n');
 
 fprintf(fid,'oModule.AssignLumpedPort Array("NAME:2", "Objects:=", Array("Feed2"), "RenormalizeAllTerminals:=",  _\n');
 fprintf(fid,'  true, "DoDeembed:=", false, Array("NAME:Modes", Array("NAME:Mode1", "ModeNum:=", 1, "UseIntLine:=",  _\n');
-fprintf(fid,'  true, Array("NAME:IntLine", "Start:=", Array("%fmm", "0mm", "0mm"), "End:=", Array( _\n',feed_pos2+feed_linew/2);
-fprintf(fid,'  "%fmm", "0mm", "%fmm")), "CharImp:=", "Zpi", "AlignmentGroup:=", 0)), "ShowReporterFilter:=",  _\n',feed_pos2+feed_linew/2, 0.5);
+fprintf(fid,'  true, Array("NAME:IntLine", "Start:=", Array("%fmm", "%dmm", "0mm"), "End:=", Array( _\n',Antenna.feed_pos2,Antenna.gnd_clearance);
+fprintf(fid,'  "%fmm", "%dmm", "%fmm")), "CharImp:=", "Zpi", "AlignmentGroup:=", 0)), "ShowReporterFilter:=",  _\n',Antenna.feed_pos2,Antenna.gnd_clearance, 0.5);
 fprintf(fid,'  false, "ReporterFilter:=", Array(true), "FullResistance:=", "50ohm", "FullReactance:=",  _\n');
 fprintf(fid,'  "0ohm")\n');
 
 % NF box
-fprintf(fid,'oEditor.CreateBox Array("NAME:BoxParameters", "XPosition:=", "-%dmm", "YPosition:=",  _\n',gndx/2+lambda/8);
-fprintf(fid,'  "-%dmm", "ZPosition:=", "-%dmm", "XSize:=", "%dmm", "YSize:=", "%dmm", "ZSize:=",  _\n',lambda/8,lambda/16,gndx+lambda/4,gndy+lambda/4);
-fprintf(fid,'  "%dmm"), Array("NAME:Attributes", "Name:=", "Box_NF", "Flags:=", "", "Color:=",  _\n',lambda/8+patchh+0.5);
+fprintf(fid,'oEditor.CreateBox Array("NAME:BoxParameters", "XPosition:=", "-%dmm", "YPosition:=",  _\n',Antenna.gndx/2+lambda/8);
+fprintf(fid,'  "-%dmm", "ZPosition:=", "-%dmm", "XSize:=", "%dmm", "YSize:=", "%dmm", "ZSize:=",  _\n',lambda/8,lambda/16,Antenna.gndx+lambda/4,Antenna.gndy+lambda/4);
+fprintf(fid,'  "%dmm"), Array("NAME:Attributes", "Name:=", "Box_NF", "Flags:=", "", "Color:=",  _\n',lambda/8+Antenna.patchh+0.5);
 fprintf(fid,'  "(132 132 193)", "Transparency:=", 0.9, "PartCoordinateSystem:=", "Global", "UDMId:=",  _\n');
 fprintf(fid,'  "", "MaterialValue:=", "" & Chr(34) & "vacuum" & Chr(34) & "", "SolveInside:=",  _\n');
 fprintf(fid,'  true)\n');
 
 %Radiating box
-fprintf(fid,'oEditor.CreateBox Array("NAME:BoxParameters", "XPosition:=", "-%dmm", "YPosition:=",  _\n',gndx/2+lambda/4);
-fprintf(fid,'  "-%dmm", "ZPosition:=", "-%dmm", "XSize:=", "%dmm", "YSize:=", "%dmm", "ZSize:=",  _\n',lambda/4,lambda/4,gndx+lambda/2,gndy+lambda/2);
-fprintf(fid,'  "%dmm"), Array("NAME:Attributes", "Name:=", "Rad_Box", "Flags:=", "", "Color:=",  _\n',lambda/2+patchh+0.5);
+fprintf(fid,'oEditor.CreateBox Array("NAME:BoxParameters", "XPosition:=", "-%dmm", "YPosition:=",  _\n',Antenna.gndx/2+lambda/4);
+fprintf(fid,'  "-%dmm", "ZPosition:=", "-%dmm", "XSize:=", "%dmm", "YSize:=", "%dmm", "ZSize:=",  _\n',lambda/4,lambda/4,Antenna.gndx+lambda/2,Antenna.gndy+lambda/2);
+fprintf(fid,'  "%dmm"), Array("NAME:Attributes", "Name:=", "Rad_Box", "Flags:=", "", "Color:=",  _\n',lambda/2+Antenna.patchh+0.5);
 fprintf(fid,'  "(132 132 193)", "Transparency:=", 0.9, "PartCoordinateSystem:=", "Global", "UDMId:=",  _\n');
 fprintf(fid,'  "", "MaterialValue:=", "" & Chr(34) & "vacuum" & Chr(34) & "", "SolveInside:=",  _\n');
 fprintf(fid,'  true)\n');
@@ -157,14 +157,14 @@ fprintf(fid,'  false, "UseAdaptiveIE:=", false, "IncludeInPostproc:=", true)\n')
 
 
 % Add a Solution Setup.
-    hfssInsertSolution(fid, 'Setup', fC/1e9);
+    hfssInsertSolution(fid, 'Setup', Antenna.fC/1e9);
     hfssInterpolatingSweep(fid, 'Sweep', 'Setup', ...
-        fLow/1e9, fHigh/1e9, nPoints);
+        Antenna.fLow/1e9, Antenna.fHigh/1e9, Antenna.nPoints);
     
     
-  fprintf(fid,'  oModule.EditSetup "Setup", Array("NAME:Setup", "Frequency:=", "%dGHz", "PortsOnly:=",  _\n',fC/1e9);
+  fprintf(fid,'  oModule.EditSetup "Setup", Array("NAME:Setup", "Frequency:=", "%dGHz", "PortsOnly:=",  _\n',Antenna.fC/1e9);
   fprintf(fid,'false, "MaxDeltaS:=", 0.02, "UseMatrixConv:=", false, "MaximumPasses:=", 25, "MinimumPasses:=",  _\n'); % Max Number of Passes
-  fprintf(fid,'3, "MinimumConvergedPasses:=", 3, "PercentRefinement:=", 20, "IsEnabled:=",  _\n'); % Min Number of Passes
+  fprintf(fid,'2, "MinimumConvergedPasses:=", 2, "PercentRefinement:=", 20, "IsEnabled:=",  _\n'); % Min Number of Passes
   fprintf(fid,'true, "BasisOrder:=", 1, "UseIterativeSolver:=", false, "DoLambdaRefine:=",  _\n');
   fprintf(fid,'true, "DoMaterialLambda:=", true, "SetLambdaTarget:=", false, "Target:=",  _\n');
   fprintf(fid,'0.3333, "UseMaxTetIncrease:=", false, "PortAccuracy:=", 2, "UseABCOnPort:=",  _\n');
@@ -177,10 +177,6 @@ fprintf(fid,'  false, "UseAdaptiveIE:=", false, "IncludeInPostproc:=", true)\n')
  hfssInsertFarFieldSphereSetup(fid, 'Radiation', [0, 180, 10], [0, 360, 10]);
  hfssCreateReport(fid, '3Drad', 5, 6, 'Setup', [],'Radiation', [], {'Theta', 'Phi', 'Freq'},{'Theta', 'Phi', 'dB(RadiationEfficiency)'});
  
-%   fprintf(fid,' oModule.CreateReport "XY Plot 1", "Far Fields", "Rectangular Plot", _\n' );
-%    fprintf(fid,' "Setup900MHz : LastAdaptive", Array("Context:=", "Infinite Sphere1"), Array("Theta:=", Array( _\n' );
-%    fprintf(fid,' "All"), "Phi:=", Array("0deg"), "Freq:=", Array("0.9GHz")), Array("X Component:=",  _\n' );
-%    fprintf(fid,' "Theta", "Y Component:=", Array("dB(RadiationEfficiency)")), Array()\n' );
     % Save the project to a temporary file and solve it.
     hfssSaveProject(fid, tmpPrjFile, true);
     hfssSolveSetup(fid, 'Setup');
