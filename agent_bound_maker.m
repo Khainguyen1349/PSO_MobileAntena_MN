901402f2bf9494aab8415b3dc7b70945dcda6021
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-09-16 10:15:18 +0200

    Update agent_bound_maker.m
    Utilizing Struct for PSO boundary variables

--- agent_bound_maker.m
+++ agent_bound_maker.m
@@ -1,14 +1,13 @@
-
-function [agent_bound, velocity_bound]= agent_bound_maker(parameter1, parameter2, parameter3)
+function [agent_bound, velocity_bound]= agent_bound_maker(parameter)
 %% How to assign boundary?
-agent_bound(:,1) = parameter1;
-agent_bound(:,2) = parameter2;
-agent_bound(:,3) = parameter3;
+agent_bound(:,1) = parameter.p1;
+agent_bound(:,2) = parameter.p2;
+agent_bound(:,3) = parameter.p3;
 
 %% How to assign velocity boundary?
-range_parameter1 = [(parameter1(1)-parameter1(2)); (parameter1(2)-parameter1(1))];
-range_parameter2 = [(parameter2(1)-parameter2(2)); (parameter2(2)-parameter2(1))];
-range_parameter3 = [(parameter3(1)-parameter3(2)); (parameter3(2)-parameter3(1))];
+range_parameter1 = [(parameter.p1(1)-parameter.p1(2)); (parameter.p1(2)-parameter.p1(1))];
+range_parameter2 = [(parameter.p2(1)-parameter.p2(2)); (parameter.p2(2)-parameter.p2(1))];
+range_parameter3 = [(parameter.p3(1)-parameter.p3(2)); (parameter.p3(2)-parameter.p3(1))];
 
 velocity_bound(:,1) = range_parameter1;
 velocity_bound(:,2) = range_parameter2;
